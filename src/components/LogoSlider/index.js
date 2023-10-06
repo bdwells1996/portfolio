50c5f9c4389fed228af43d7f3d071d7ad9e6dc76
bdwells1996 <bdwells1996@gmail.com>
2023-10-06 14:40:55 +0100

    WIP: New assets for slider

--- src/components/LogoSlider/index.js
+++ src/components/LogoSlider/index.js
@@ -1,5 +1,13 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {  Autoplay } from 'swiper/modules';
+import LogoGitHub from '../../assets/images/github-logo.png';
+import LogoAdobe from '../../assets/images/adobe-logo.png';
+import LogoCss from '../../assets/images/css-logo.png';
+import LogoFigma from '../../assets/images/figma-logo.png';
+import LogoHtml from '../../assets/images/html-logo.png';
+import LogoJs from '../../assets/images/js-logo.png';
+import LogoReact from '../../assets/images/react-logo.png';
+import LogoSass from '../../assets/images/sass-logo.png';
 
 import './index.scss';
 import 'swiper/css';
@@ -12,18 +20,44 @@ const LogoSlider = () => {
     <div className="c-logo-slider">
         <Swiper 
         modules={[ Autoplay ]}
-        spaceBetween={40}
+        spaceBetween={20}
         autoPlay={{
             delay: 2000
         }}
         slidesPerView={4}
         className="c-logo-slider__inner">
-            <SwiperSlide className="c-logo-slider__inner__slide">Slide 1</SwiperSlide>
-            <SwiperSlide className="c-logo-slider__inner__slide">Slide 2</SwiperSlide>
-            <SwiperSlide className="c-logo-slider__inner__slide">Slide 3</SwiperSlide>
-            <SwiperSlide className="c-logo-slider__inner__slide">Slide 4</SwiperSlide>
-            <SwiperSlide className="c-logo-slider__inner__slide">Slide 5</SwiperSlide>
-            <SwiperSlide className="c-logo-slider__inner__slide">Slide 6</SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoReact } alt="logo" /> 
+                React
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoGitHub } alt="logo" /> 
+                Github
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoJs } alt="logo" /> 
+                Javascript
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoSass } alt="logo" /> 
+                SASS/SCSS
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoFigma } alt="logo" /> 
+                Figma
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoAdobe } alt="logo" /> 
+                Adobe Suite
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoHtml } alt="logo" /> 
+                HTML5
+            </SwiperSlide>
+            <SwiperSlide className="c-logo-slider__inner__slide">
+                <img src={ LogoCss } alt="logo" /> 
+                CSS3
+            </SwiperSlide>
         </Swiper>
         </div>
       
